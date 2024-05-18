68e725746ecce109c45420ecc2cba4a8701b8600
luc-git-projects <luc.git.projects@hotmail.com>
2024-05-17 23:06:29 -0300

    tipo boolean

---
+++ output/boolean.js
@@ -0,0 +1,19 @@
+"use strict";
+let admin = true;
+admin = false;
+/**
+ * convertendo 1 ou 0 respectivamente para true ou false
+ */
+admin = Boolean("1");
+console.log(admin);
+admin = Boolean(0);
+console.log(admin);
+admin = Boolean("2");
+console.log(admin);
+admin = Boolean("");
+console.log(admin);
+admin = Boolean(null);
+console.log(admin);
+admin = Boolean(undefined);
+console.log(admin);
+//# sourceMappingURL=boolean.js.map
