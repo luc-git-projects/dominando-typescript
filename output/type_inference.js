3847d8ab96cb66883cd645ea12cdf1bc1c33ede5
luc-git-projects <luc.git.projects@hotmail.com>
2024-05-17 21:58:00 -0300

    inferencia de tipos

--- output/type_inference.js
+++ output/type_inference.js
@@ -1,4 +1,7 @@
 "use strict";
 var linguagem = "JavaScript";
 console.log(linguagem);
+//linguagem = 2015;
+//console.log(linguagem.toFixed());
+console.log(linguagem.toLocaleUpperCase());
 //# sourceMappingURL=type_inference.js.map
--- scripts/type_inference.ts
+++ scripts/type_inference.ts
@@ -2,4 +2,15 @@ let linguagem = "JavaScript";
 
 console.log(linguagem)
 
+
 //linguagem = 2015;
+
+//console.log(linguagem.toFixed());
+
+console.log(linguagem.toLocaleUpperCase());
+
+const usuarioInfo = [1, "Luciano", new Date()];
+
+usuarioInfo.push(2);
+usuarioInfo.push("Carlos");
+usuarioInfo.push(new Date());
